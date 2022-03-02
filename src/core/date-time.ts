a33bdf8ef9b203922faa5c7ad95d4135cce8656f
Prajwal264 <prajwalp264@gmail.com>
2022-03-02 20:43:49 +0530

    (feat): isLeapyear - added conditions to handle different types of input

--- src/core/date-time.ts
+++ src/core/date-time.ts
@@ -1,17 +1,46 @@
 import { Weekdays } from '../enums/weekdays.enum';
+import { isString, isValid } from '../helpers/type.helper';
 
+/**
+ *
+ *
+ * @class DateTime
+ * @extends {Date}
+ */
 class DateTime extends Date {
+  /**
+   * Return an array of all the values of the Weekdays enum
+   * @returns An array of Weekdays
+   */
   public static getWeekdays(): Weekdays[] {
     return Object.values(Weekdays)
   }
 
-  public static isLeapYear(year: number): boolean { // https://docs.microsoft.com/en-us/office/troubleshoot/excel/determine-a-leap-year
-    if(year % 4 === 0) { 
-      if(year % 100 !== 0) { 
+  /**
+   * Given a year, return true if it is a leap year, false otherwise
+   * @param {number} year - The year to check.
+   * @returns A boolean value.
+   */
+  public static isLeapYear(year?: number | Date | string): boolean { // https://docs.microsoft.com/en-us/office/troubleshoot/excel/determine-a-leap-year
+    let yearToBeChecked = new Date().getFullYear();
+    if(year) {
+      if(year instanceof Date) {
+        yearToBeChecked = year.getFullYear();
+      } else if(isString(year)) {
+        if(isValid(year)) {
+          yearToBeChecked = new Date(year).getFullYear();
+        } else {
+          // TODO: invalid date
+          return false;
+        }
+      } 
+    }
+    if(yearToBeChecked % 4 === 0) { 
+      if(yearToBeChecked % 100 !== 0) { 
         return true;
       }
     }
-    if(year % 400 === 0) {
+    if(yearToBeChecked % 400 === 0) {
       return true;
     }
     return false;
